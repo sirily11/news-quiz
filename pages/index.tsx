c8f54146f8cadad3a67b04cfba05715c995f0cb5
sirily11 <sirily1997@gmail.com>
2022-09-07 22:05:24 +0500

    feat: add new home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,30 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { Box, Stack, Typography } from "@mui/material";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div>
-   
-    </div>
-  )
-}
+    <Box sx={{ background: "#795548", height: "100vh" }}>
+      <Head>
+        <title>Twinkle coffee</title>
+      </Head>
+      <Stack
+        direction={"column"}
+        justifyContent="center"
+        alignContent={"center"}
+        alignItems={"center"}
+        height={"100%"}
+      >
+        <Box>
+          <Image src={"/coffee.webp"} height={300} width={300} />
+        </Box>
+        <Typography color={"white"} fontSize={30}>
+          Coffee with Twinkle and siri!
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
